78f9a190d860eb57d1b5f932249bf03052331f31
Duncan Graham Walker <walkerdu@bc.edu>
2015-10-05 21:48:06 -0400

    Importing compass mixins

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,9 @@
+var lint = require('sass-lint')
 var path = require('path');
 var sassTrue = require('sass-true');
+
 var sassFile = path.join(__dirname, 'dummy.scss');
 
-sassTrue.runSass({file: sassFile}, describe, it);
+sassTrue.runSass({ file: sassFile }, describe, it);
+
+// TODO - sass linting
