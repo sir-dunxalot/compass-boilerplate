6c784948c8ef88d9710064a026609372558d43f7
Duncan Graham Walker <walkerdu@bc.edu>
2015-10-05 22:48:26 -0400

    More informative linting output

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,27 @@
-var lint = require('sass-lint')
+var chalk = require('chalk');
+var lint = require('sass-lint');
 var path = require('path');
 var sassTrue = require('sass-true');
 
-var sassFile = path.join(__dirname, 'dummy.scss');
+var dummyFilePath = path.join(__dirname, 'dummy.scss');
+var sassLintOptions = {
+  configFile: 'sass-lint.yml',
+}
 
-sassTrue.runSass({ file: sassFile }, describe, it);
+/* Lint the lib and test files */
 
-// TODO - sass linting
+var results = lint.lintFiles('{lib,tests}/**/*.scss', sassLintOptions, sassLintOptions.configFile);
+var formattedResults = lint.format(results);
+
+/* Log the linting results */
+
+if (!formattedResults) {
+  console.log(chalk.white('  Linting'));
+  console.log(chalk.green('    ✓  No SASS linting errors - you rock!'));
+} else {
+  console.log(lint.format(results));
+}
+
+/* Then run the test suite */
+
+sassTrue.runSass({ file: dummyFilePath }, describe, it);
